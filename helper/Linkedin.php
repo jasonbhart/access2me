a2abf483420fa396e6849f53dc10b79e7f69699c
yup <yup@tane.edu.ua>
2014-10-21 13:21:41 +0300

    Fixed Linkedin profile parsing

--- helper/Linkedin.php
+++ helper/Linkedin.php
@@ -14,6 +14,51 @@ class Linkedin
         $this->config = $config;
     }
 
+    public function parseProfileData($data)
+    {
+        $person = $data[0];
+        $profile = array();
+        
+        $profile['first_name'] = isset($person->{'first-name'}) ? (string) $person->{'first-name'} : null;
+        $profile['last_name'] = isset($person->{'last-name'}) ? (string) $person->{'last-name'} : null;
+        $profile['email'] = isset($person->{'email-address'}) ? (string) $person->{'email-address'} : null;
+        $profile['headline'] = isset($person->{'headline'}) ? (string) $person->{'headline'} : null;
+        $profile['picture_url'] = isset($person->{'picture-url'}) ? (string) $person->{'picture-url'} : null;
+        $profile['profile_url'] = isset($person->{'site-standard-profile-request'}->{'url'}) ? (string) $person->{'site-standard-profile-request'}->{'url'} : null;
+        $profile['location'] = isset($person->{'location'}->{'name'}) ? (string) $person->{'location'}->{'name'} : null;
+        $profile['industry'] = isset($person->{'industry'}) ? (string) $person->{'industry'} : null;
+        $profile['self_summary'] = isset($person->{'summary'}) ? (string) $person->{'summary'} : null;
+        $profile['specialties'] = isset($person->{'specialties'}) ? (string) $person->{'specialties'} : null;
+        $profile['associations'] = isset($person->{'associations'}) ? (string) $person->{'associations'} : null;
+        $profile['interests'] = isset($person->{'interests'}) ? (string) $person->{'interests'} : null;
+        $profile['total_connections'] = isset($person->{'num-connections'}) ? (string) $person->{'num-connections'} : null;
+
+        // parse positions
+        $positions = array();
+        if (isset($person->positions)
+            && isset($person->positions->position)
+        ) {
+            foreach ($person->positions->position as $position) {
+                $item = array();
+                
+                if (isset($position->company) && isset($position->company->name)) {
+                    $item['company'] = (string)$position->company->name;
+                }
+
+                $item['title'] = isset($position->title) ? (string)$position->title : NULL;
+                $item['summary'] = isset($position->summary) ? (string)$position->summary : NULL;
+                $item['is_current'] = isset($position->{'is-current'})
+                    ? (string)$position->{'is-current'} : false;
+                
+                $positions[] = $item;
+            }
+        }
+
+        $profile['positions'] = $positions;
+
+        return $profile;
+    }
+
     public function getProfile($token)
     {
         $url = "https://api.linkedin.com/v1/people/~:(";
@@ -55,30 +100,7 @@ class Linkedin
         $xml = new \SimpleXMLElement($result);
         $data = $xml->xpath('/person');
 
-        $profile = array();
-        
-        $profile['first_name'] = (isset($data[0]->{'first-name'})) ? (string) $data[0]->{'first-name'} : null;
-        $profile['last_name'] = (isset($data[0]->{'last-name'})) ? (string) $data[0]->{'last-name'} : null;
-        $profile['email'] = (isset($data[0]->{'email-address'})) ? (string) $data[0]->{'email-address'} : null;
-        $profile['headline'] = (isset($data[0]->{'headline'})) ? (string) $data[0]->{'headline'} : null;
-        $profile['picture_url'] = (isset($data[0]->{'picture-url'})) ? (string) $data[0]->{'picture-url'} : null;
-        $profile['profile_url'] = (isset($data[0]->{'site-standard-profile-request'}->{'url'})) ? (string) $data[0]->{'site-standard-profile-request'}->{'url'} : null;
-        $profile['location'] = (isset($data[0]->{'location'}->{'name'})) ? (string) $data[0]->{'location'}->{'name'} : null;
-        $profile['industry'] = (isset($data[0]->{'industry'})) ? (string) $data[0]->{'industry'} : null;
-        $profile['self_summary'] = (isset($data[0]->{'summary'})) ? (string) $data[0]->{'summary'} : null;
-        $profile['specialties'] = (isset($data[0]->{'specialties'})) ? (string) $data[0]->{'specialties'} : null;
-        $profile['associations'] = (isset($data[0]->{'associations'})) ? (string) $data[0]->{'associations'} : null;
-        $profile['interests'] = (isset($data[0]->{'interests'})) ? (string) $data[0]->{'interests'} : null;
-        $profile['total_connections'] = (isset($data[0]->{'num-connections'})) ? (string) $data[0]->{'num-connections'} : null;
-        $profile['total_positions'] = (isset($data[0]->{'positions'}->attributes()['total'][0])) ? (string) $data[0]->{'positions'}->attributes()['total'][0] : null;
-
-        for ($x = 0; $x < $profile['total_positions']; $x++) {
-            $profile['positions'][$x]['company'] = (string) $data[0]->{'positions'}->{'position'}[$x]->{'company'}->{'name'};
-            $profile['positions'][$x]['title'] = (string) $data[0]->{'positions'}->{'position'}[$x]->{'title'};
-            $profile['positions'][$x]['summary'] = (string) $data[0]->{'positions'}->{'position'}[$x]->{'summary'};
-            $profile['positions'][$x]['is_current'] = (string) $data[0]->{'positions'}->{'position'}[$x]->{'is-current'};
-        }
-        
+        $profile = $this->parseProfileData($data);
         return $profile;
     }
 }
